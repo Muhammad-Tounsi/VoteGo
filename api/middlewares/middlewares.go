7eefd47984f275be0f03060fe8b2fe3ee920b848
saneto <aymanezaircha@gmail.com>
2019-11-12 14:26:29 +0100

    Feat controler, Routes and middleware

---
+++ api/middlewares/middlewares.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/Muhammad-Tounsi/Vote-Go-Vue/api/auth"
+	"github.com/Muhammad-Tounsi/Vote-Go-Vue/api/responses"
+)
+
+// SetMiddlewareJSON : This will format all responses to JSON
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+// SetMiddlewareAuthentication : will check for the validity of the authentication token provided
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := auth.TokenValid(r)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
